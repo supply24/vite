2c371ba9e1fa6264f6e22e8f981b660122e84a34
Tobias <tobiasmautner@outlook.com>
2026-02-16 09:28:31 +0100

    s

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import { qrcode } from 'vite-plugin-qrcode';
+//import { qrcode } from 'vite-plugin-qrcode'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   //plugins: [qrcode()],
 
-})
+}
+
+
+)
