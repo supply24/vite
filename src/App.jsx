e5a0a3e7016dde2deb6aead5b638f1e01d71947f
Tobias <tobiasmautner@outlook.com>
2026-02-16 10:05:17 +0100

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <Button variant="alert">1st button </Button>
+      <Button variant="secondary">1st button </Button>
       <a href="https://drive.google.com/file/d/1wz-HmN82MHwJ7hPfSv4sWmtBed9lAiVb/view?usp=sharing">Download</a>
     </>
   );
