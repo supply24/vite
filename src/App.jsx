49f9f130e9276fbe623fa483590e47713299e41f
Tobias <tobiasmautner@outlook.com>
2026-02-16 08:12:00 +0100

    w

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button variant="alert">1st button </Button>
+    </>
+  );
+}
+
+export default App;
