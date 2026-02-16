2aabd99ed22c01c032ca6bcf5c5afc84bd419caf
Tobias <tobiasmautner@outlook.com>
2026-02-16 10:07:34 +0100

    t

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <Button variant="secondary">1st button </Button>
+      <Button variant="success">1st button </Button>
       <a href="https://drive.google.com/file/d/1wz-HmN82MHwJ7hPfSv4sWmtBed9lAiVb/view?usp=sharing">Download</a>
     </>
   );
